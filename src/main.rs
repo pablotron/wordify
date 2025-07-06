95fe157a6f3d672c9ab876b1973513aa152734e9
Paul Duncan <pabs@pablotron.org>
2025-07-06 07:28:27 -0400

    src/main.rs: minor comment fix

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), std::io::Error> {
   let t: madlib::Template = serde_json::from_reader(std::io::stdin())?; // read template
   let mut buf = t.expand(&mut rand::rng())?; // expand template
   if buf.is_empty() || buf[buf.len() - 1] != b'\n' {
-    writeln!(&mut buf)? // write trailing newline
+    writeln!(&mut buf)? // append newline
   }
 
   std::io::stdout().lock().write_all(&buf)?; // write buffer
